24eacfcb6dcfbc6600eb58b692a4974d284c7cb3
Arespeak <785565314@qq.com>
2020-12-02 00:59:23 +0800

    网页模板

--- sim/urls.py
+++ sim/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.content),
-    url(r'^index/', views.index),
+    url(r'^index/$', views.index),
     url(r'^add/$', views.add),
     url(r'^edit/$', views.edit),
     url(r'^delete/$', views.delete)
